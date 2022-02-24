b7032f4bcb9f6c31c829c86269f0d256d85c4bb1
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-02-24 11:58:33 +0100

    ProjectCard style

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,16 +5,21 @@ function ProjectCard({ project }) {
   const image = require(`../assets/${project.image}`).default;
 
   return (
-    <div className={styles.projectCard}>
-      <img src={image} className={styles.image} alt="project"></img>
-      <div className={styles.detailsContainer}>
-        <p className={styles.name}>{project.name}</p>
-        <p className={styles.description}>{project.description}</p>
-        <a href={project.github} className={styles.githubLink} target='_blank' rel="noreferrer">
-          Github repository
-        </a>
+    <a
+      href={project.github}
+      className={styles.githubLink}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <div className={styles.projectCard}>
+        <img src={image} className={styles.image} alt='project'></img>
+        <div className={styles.detailsContainer}>
+          <p className={styles.name}>{project.name}</p>
+          <p className={styles.description}>{project.description}</p>
+          <p className={styles.githubLink}>Github repository</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
